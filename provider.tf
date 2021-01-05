b4f1f5e0e3f9dd4d9a56a9d30a8980a902543111
Niharika Thimmareddy <niharika.bhoomi15@gmail.com>
2021-01-05 16:59:39 -0500

    add variables

--- provider.tf
+++ provider.tf
@@ -2,3 +2,19 @@ provider "azurerm" {
   version = "~>2.0"
   features {}
 }
+
+variable "subscription_id" {
+  description = "The Subscription Id of Azure"
+}
+
+variable "client_id" {
+  description = "Client id of the app that you registered in AAD"
+}
+
+variable "client_secret" {
+  description = "Client secret of the app that you registered in AAD"
+}
+
+variable "tenant_id" {
+  description = "tenant id of the app that you registered in AAD"
+}
