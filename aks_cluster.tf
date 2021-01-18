26fe5a2df530993f615adfb58766c8f0080fba66
Niharika Thimmareddy <niharika.bhoomi15@gmail.com>
2021-01-18 17:17:57 -0500

    edited again duffa

--- aks_cluster.tf
+++ aks_cluster.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "k8s" {
 
 resource "azurerm_kubernetes_cluster" "k8s" {
   name                = "aks_cluster"
-  location            = "azurerm_resource_group.k8s.location"
-  resource_group_name = "azurerm_resource_group.k8s.name"
+  location            = azurerm_resource_group.k8s.location
+  resource_group_name = azurerm_resource_group.k8s.name
   dns_prefix          = "aksclusterex"
 
 default_node_pool {
