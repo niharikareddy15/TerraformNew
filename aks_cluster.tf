0a277ffeb75e3cfd64ed9039e6af117cb7e898cc
Niharika Thimmareddy <niharika.bhoomi15@gmail.com>
2021-01-18 17:09:18 -0500

    creating AKS cluster

---
+++ aks_cluster.tf
@@ -0,0 +1,28 @@
+resource "azurerm_resource_group" "k8s" {
+  name     = "aks_rg"
+  location = "eastus"
+}
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = "aks_cluster"
+  location            = "azurerm_resource_group.k8s.location"
+  resource_group_name = "azurerm_resource_group.k8s.name"
+  dns_prefix          = "aksclusterex"
+
+default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_D2_v2"
+  }
+  tags {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+    value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+    value = azurerm_kubernetes_cluster.k8s.kube_config_raw
+}
