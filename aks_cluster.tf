b5c320d0d1af16bd61fea18443a9757b8a38b619
Niharika Thimmareddy <niharika.bhoomi15@gmail.com>
2021-01-18 18:20:01 -0500

    update name

--- aks_cluster.tf
+++ aks_cluster.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "k8s" {
 }
 
 resource "azurerm_kubernetes_cluster" "k8s" {
-  name                = "aks_cluster"
+  name                = "aks"
   location            = azurerm_resource_group.k8s.location
   resource_group_name = azurerm_resource_group.k8s.name
   dns_prefix          = "aksclusterex"
